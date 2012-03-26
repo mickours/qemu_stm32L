7548405923fcc740420baaeec99a8b3532d4707c
Tetram <aurelien@bip3.localdomain>
2012-03-26 12:47:44 +0200

    Interface plus fonctionnelle mais non finale

--- srcInterface/UI.py
+++ srcInterface/UI.py
@@ -1,4 +1,5 @@
 import Tkinter
+import socket
 from Tkinter import PhotoImage
 
 class UI(Tkinter.Tk):
@@ -31,7 +32,7 @@ class UI(Tkinter.Tk):
         self.labelIP.grid(column=0,row=0,sticky='EW')
         
         self.entryVariableIP = Tkinter.StringVar()
-        self.entryVariableIP.set("Entrez l'@IP ici")
+        self.entryVariableIP.set("Not supported")
         self.entryIP = Tkinter.Entry(self,textvariable=self.entryVariableIP)
         self.entryIP.grid(column=0,row=1,sticky='EW')
         
@@ -39,15 +40,15 @@ class UI(Tkinter.Tk):
         self.labelPort.grid(column=0,row=2,sticky='EW') 
         
         self.entryVariablePort = Tkinter.StringVar()
-        self.entryVariablePort.set(u"Entrez le port ici")
+        self.entryVariablePort.set(u"4242")
         self.entryPort = Tkinter.Entry(self,textvariable=self.entryVariablePort)
         self.entryPort.grid(column=0,row=3,sticky='EW')
         
-        button = Tkinter.Button(self,text="Connexion",command=self.OnButtonConnexionClick)
+        button = Tkinter.Button(self,text="Lancer",command=self.OnButtonConnexionClick)
         button.grid(column=0,row=4)
         
         self.labelVariableCo = Tkinter.StringVar()
-        self.labelVariableCo.set(u"-----------")
+        self.labelVariableCo.set(u"Non connecte")
         self.labelConnexion = Tkinter.Label(self,textvariable=self.labelVariableCo)
         self.labelConnexion.grid(column=0,row=5,sticky='EW')
         
@@ -71,20 +72,51 @@ class UI(Tkinter.Tk):
         #self.geometry(self.geometry())
         
     def OnButtonConnexionClick(self):
-        self.canvas.itemconfigure(self.ledGauche, image=self.ledBleue)
-        self.canvas.itemconfigure(self.ledDroite, image=self.ledVerte)
-        self.labelVariableCo.set(self.entryVariablePort.get())
+        self.labelVariableCo.set("Initialisation...")
+        app.clicked()
         
-    
     def apropos(self):
         pass
     
     def use(self):
         pass
+
+class Ctrl:
+    def __init__(self,parent):
+        self.ui = UI(parent)
         
+        
+    def traiterrecv(self):
+        self.ui.canvas.itemconfigure(self.ledGauche, image=self.ledBleue)
+        self.ui.canvas.itemconfigure(self.ledDroite, image=self.ledVerte)
+        
+        while 1:
+            data = self.conn.recv(4)
+            if not data: break
+    
+            index = 0
+            while index < len(data):
+                print ord(data[index])
+                index = index +1
+        
+        
+    def clicked(self):
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.bind((socket.gethostname(), int(self.ui.entryVariablePort.get())))
+        self.serversocket.listen(1)
+        self.ui.labelVariableCo.set(u"En attente de connexion \nsur le port " + self.ui.entryVariablePort.get())
+        self.ui.update()
+        self.conn, self.addr = self.serversocket.accept()
+        self.ui.labelVariableCo.set("Connecte a : " +self.addr)
+        self.ui.update()
+        self.traiterrecv()
+        self.conn.close()
+        self.ui.labelVariableCo.set("Non connecte")
+        
+           
     
 if __name__ == "__main__":
-    app = UI(None)
-    app.title('STM32LDiscovery UI')
-    app.mainloop()
+    app = Ctrl(None)
+    app.ui.title('STM32LDiscovery UI')
+    app.ui.mainloop(0)
         
