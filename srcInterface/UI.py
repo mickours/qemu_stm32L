d50add97e131c08375c9a98252f6d124ad097a4f
Tetram <aurelien@bip3.localdomain>
2012-04-02 15:51:18 +0200

    Bouton User

--- srcInterface/UI.py
+++ srcInterface/UI.py
@@ -1,6 +1,15 @@
+
+##################################################################
+#     Interface pour l'emulation de la carte STM32LDiscovery     #  
+#                            avec Qemu                           #
+#        EID Timothee - MERCIER Michael - CLAVELIN Aurelien      #
+##################################################################
+
 import Tkinter
 import socket
 import threading
+import struct
+
 from Tkinter import PhotoImage
 
 class UI(Tkinter.Tk):
@@ -17,7 +26,6 @@ class UI(Tkinter.Tk):
         
         self.fichier = Tkinter.Menu(tearoff=0)
         self.menu1.add_cascade(label="Fichier",menu=self.fichier)
-        #self.fichier.add_command(label="  Reinitialiser", command=app.reinit)
         self.fichier.add_command(label="  Quitter", command=self.quitter)
         self.fichier = Tkinter.Menu(tearoff=0)
         self.menu1.add_cascade(label="Aide",menu=self.fichier)
@@ -29,6 +37,7 @@ class UI(Tkinter.Tk):
         
         self.grid()
         
+        '''
         self.labelIP = Tkinter.Label(self,anchor='w',text="IP :")
         self.labelIP.grid(column=0,row=0,sticky='EW')
         
@@ -36,6 +45,7 @@ class UI(Tkinter.Tk):
         self.entryVariableIP.set("Not supported")
         self.entryIP = Tkinter.Entry(self,textvariable=self.entryVariableIP)
         self.entryIP.grid(column=0,row=1,sticky='EW')
+        '''
         
         self.labelPort = Tkinter.Label(self,anchor='w',text="Port :")
         self.labelPort.grid(column=0,row=2,sticky='EW') 
@@ -46,27 +56,39 @@ class UI(Tkinter.Tk):
         self.entryPort.grid(column=0,row=3,sticky='EW')
         
         button = Tkinter.Button(self,text="Lancer",command=self.OnButtonConnexionClick)
+        button.grid(column=0,row=4,sticky="EW")
         
-        button.grid(column=0,row=4)
+        self.labelLog = Tkinter.Label(self,anchor='w',text="Log : ")
+        self.labelLog.grid(column=0,row=7,sticky='EW')
         
-        self.labelVariableCo = Tkinter.StringVar()
-        self.labelVariableCo.set(u"Non connecte")
-        self.labelConnexion = Tkinter.Label(self,textvariable=self.labelVariableCo)
-        self.labelConnexion.grid(column=0,row=5,sticky='EW')
+        self.log = Tkinter.Text(width=20,height=20)
+        self.log.grid(column=0,row=8,sticky='EWNS',rowspan=50)
+        self.log.insert(Tkinter.END,"")
         
-        self.labelVariableLog = Tkinter.StringVar()
-        self.labelVariableLog.set("Log : ")
-        self.labelLog = Tkinter.Label(self,textvariable=self.labelVariableLog)
-        self.labelLog.grid(column=0,row=6,sticky='EW')
+        button2 = Tkinter.Button(self,text="Vider",command=self.Clear)
+        button2.grid(column=0,row=59,sticky="EW")
         
+        scrollbar = Tkinter.Scrollbar(self.log)
+        scrollbar.pack(side='right', fill='y')
+        
+        self.log.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.log.yview)
+
         self.carte=PhotoImage(file="carte.gif")
         self.ledBlanche=PhotoImage(file="LEDblanche.gif")
         self.ledBleue=PhotoImage(file="LEDbleue.gif")
         self.ledVerte=PhotoImage(file="LEDverte.gif")
+        self.user=PhotoImage(file="user.gif")
         
         self.canvas = Tkinter.Canvas(self,width=self.carte.width(),height=self.carte.height())
-        self.canvas.grid(column=1,row=1,rowspan=70)
-        self.cartei = self.canvas.create_image(0,0, anchor = "nw", image=self.carte)
+        self.canvas.grid(column=2,row=1,rowspan=70)
+        self.canvas.create_image(0,0, anchor = "nw", image=self.carte)
+        
+        self.canvas.create_image(70,390,image=self.user, tag="user", anchor = "nw")
+        self.canvas.create_image(150,390,image=self.user, tag="reset", anchor = "nw")
+        self.canvas.tag_bind("reset", "<Button-1>",self.Reset)
+        self.canvas.tag_bind("user", "<Button-1>",self.User)
+        
         self.ledGauche = self.canvas.create_image(35,400, anchor = "nw", image=self.ledBlanche)
         self.ledDroite = self.canvas.create_image(190,400, anchor = "nw", image=self.ledBlanche)
         
@@ -75,11 +97,24 @@ class UI(Tkinter.Tk):
         self.grid_rowconfigure(0,weight=1) #Gestion verticale de l'agrandissement
         self.resizable(False,False) #Pas redimentionable !
         
-        self.update()
+        
         #self.geometry(self.geometry())
+        self.update()
         
     def OnButtonConnexionClick(self):
         app.event.set()
+        
+    def User(self,none):
+        self.log.insert(Tkinter.END,"User button clicked\n")
+        app.user_clicked()
+        self.log.see(Tkinter.END)
+        
+    def Reset(self,none):
+        self.log.insert(Tkinter.END,"Reset button clicked\n")
+        self.log.see(Tkinter.END)
+        
+    def Clear (self):
+        self.log.delete(1.0, Tkinter.END)
     
     def quitter(self):
         if hasattr(app,'thread') :
@@ -91,7 +126,10 @@ class UI(Tkinter.Tk):
         self.update()
         
     def apropos(self):
-        pass
+        t1=Tkinter.Toplevel()
+        t1.title("A propos")
+        t1.update()
+        
     
     def use(self):
         pass
@@ -111,8 +149,11 @@ class Ctrl:
     def close_conn(self):
         if hasattr(self,'conn') :
             self.conn.close()
-            self.ui.labelVariableCo.set("Non connecte")
-        
+            self.ui.log.insert(Tkinter.END,"Non connecte\n")
+            
+    def user_clicked(self):
+        self.conn.send(struct.pack('B',1))
+        self.conn.send(struct.pack('B',0))
         
     def traiterrecv(self):
         while 1:
@@ -121,27 +162,28 @@ class Ctrl:
             if ord(data) == 0:
                 self.ui.canvas.itemconfigure(self.ui.ledGauche, image=self.ui.ledBlanche)
                 self.ui.canvas.itemconfigure(self.ui.ledDroite, image=self.ui.ledBlanche)
-                self.ui.printlog("Log : Led off") 
+                self.ui.log.insert(Tkinter.END,"Donnee recue : " + str(ord(data)) + "\n" + "=>LEDs off\n")
             elif ord(data) == 192:
                 self.ui.canvas.itemconfigure(self.ui.ledGauche, image=self.ui.ledBleue)
                 self.ui.canvas.itemconfigure(self.ui.ledDroite, image=self.ui.ledVerte)
-                self.ui.printlog("Log : Led on")
+                self.ui.log.insert(Tkinter.END,"Donnee recue : " + str(ord(data)) + "\n" + "=>LEDs on\n")
             else:
-                self.ui.printlog("Log : Data non reconnue")
+                self.ui.log.insert(Tkinter.END,"Donnee recue : " + str(ord(data)) + "\n" + "=>non reconnue\n")
+            self.ui.log.see(Tkinter.END)
             self.ui.update()
     
     def clicked(self):
-		    self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		    self.serversocket.bind((socket.gethostname(), int(self.ui.entryVariablePort.get())))
-		    self.serversocket.listen(1)
-		    self.ui.labelVariableCo.set(u"En attente de connexion \nsur le port " + self.ui.entryVariablePort.get())
-		    self.ui.update()
-		    self.conn, self.addr = self.serversocket.accept()
-		    self.ui.labelVariableCo.set("Connecte")
-		    self.ui.update()
-		    self.traiterrecv()
-		    self.close_conn()
-                
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.bind((socket.gethostname(), int(self.ui.entryVariablePort.get())))
+        self.serversocket.listen(1)
+        self.ui.log.insert(Tkinter.END,"En attente de connexion sur le port " + self.ui.entryVariablePort.get()+ "\n")
+        self.ui.update() 
+        self.conn, self.addr = self.serversocket.accept()
+        self.ui.log.insert(Tkinter.END,"Connecte \n")
+        self.ui.update()
+        self.traiterrecv()
+        self.close_conn()
+        
     def tester(self):
         self.ui.canvas.itemconfigure(self.ui.ledGauche, image=self.ui.ledBleue)
         self.ui.canvas.itemconfigure(self.ui.ledDroite, image=self.ui.ledVerte)
@@ -158,4 +200,4 @@ if __name__ == "__main__":
     app = Ctrl(None)
     app.ui.title('STM32LDiscovery UI')
     app.ui.mainloop()
-        
+    
